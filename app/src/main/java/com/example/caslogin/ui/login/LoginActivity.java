ee9cea38c5cbce735fb6d208f8d9144f316431fa
x.veiga <x.veiga@udc.es>
2019-09-26 22:59:03 +0200

    Initial NDK C++ code test

--- app/src/main/java/com/example/caslogin/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/caslogin/ui/login/LoginActivity.java
@@ -36,6 +36,17 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
     private LoginViewModel loginViewModel;
 
     @Override
@@ -125,6 +136,8 @@ public class LoginActivity extends AppCompatActivity {
                         passwordEditText.getText().toString());
             }
         });
+
+        usernameEditText.setText(stringFromJNI());
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
